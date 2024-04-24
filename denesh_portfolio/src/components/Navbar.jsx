535716a2690d69bf9662ff79e4603ad00ca0bfce
Denesh Anandathasan <denesh555@gmail.com>
2024-04-23 21:52:01 -0400

    update skilss

--- denesh_portfolio/src/components/Navbar.jsx
+++ denesh_portfolio/src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar({ scrollToSection }) {
-  const handleScrollTo = (sectionId) => {
-    scrollToSection(sectionId)
+  const handleScrollTo = (sectionRef) => {
+    window.scrollTo({
+      top: sectionRef.current.offsetTop,
+      behavior: 'smooth'
+    })
   }
 
   return (
